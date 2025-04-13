1a69a8b5e601457c7ea5e86c134e3abe24ea979e
Abir-dev <abirlalbanerjee44@gmail.com>
2025-04-14 00:29:25 +0530

    solved the problem causing the error in deployement

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,42 +1,38 @@
-
 import AppointmentForm from "@/components/forms/AppointmentForm";
-import PatientForm from "@/components/forms/PatientForm";
-import { Button } from "@/components/ui/button";
 import { getPatient } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 
-
-export default async function NewAppointment({ params: { userId }}: SearchParamProps) {
+export default async function NewAppointment({ params }: SearchParamProps) {
+  const { userId } = await params; // Await params before destructuring
   const patient = await getPatient(userId);
+  
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[860px] flex-1 justify-between">
-          <Image 
-          src="/assets/icons/logo-full.svg"
-          height={1000}
-          width={1000}
-          alt="patient"
-          className="mb-12 h-10 w-fit"
-          /> 
-
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
 
           <AppointmentForm
-            type = "create"
+            type="create"
             userId={userId}
             patientId={patient.$id}
           />
-            <p className="copyright mt-10 py-12">© 2025 CarePulse</p>
-
+          <p className="copyright mt-10 py-12">© 2025 CarePulse</p>
         </div>
       </section>
 
-      <Image 
-      src="/assets/images/appointment-img.png"
-      height={1000}
-      width={1000}
-      alt="appointment"
-      className="side-img max-w-[390px] bg-bottom "
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
       />
     </div>
   );
